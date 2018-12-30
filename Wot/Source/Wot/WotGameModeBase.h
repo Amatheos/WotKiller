ba2e131e8c7a421b02b1992ead07e61f2150e578
Amatheos <44970824+Amatheos@users.noreply.github.com>
2018-12-30 20:11:29 +0100

    Moved gitignore

---
+++ Wot/Source/Wot/WotGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WotGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WOT_API AWotGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
