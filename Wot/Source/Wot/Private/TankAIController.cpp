58d006cc5d3fedd9884f0fcd611c32cc55d5b7a0
Amatheos <44970824+Amatheos@users.noreply.github.com>
2019-01-04 15:19:03 +0100

    Player and AI possession works

---
+++ Wot/Source/Wot/Private/TankAIController.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) { UE_LOG(LogTemp, Warning, TEXT("AI does not possess a tank")) }
+	else { UE_LOG(LogTemp, Warning, TEXT("AI possesses %s"), *(ControlledTank->GetName())) }
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
